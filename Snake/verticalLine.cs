99c1df0b537f56f97a59e284b1fc1e58ca2ed1f0
дааниэль логинов <daaniel124@gmail.com>
2022-01-02 15:42:27 +0200

    class Figure - наследование

--- Snake/verticalLine.cs
+++ Snake/verticalLine.cs
@@ -4,10 +4,8 @@ using System.Text;
 
 namespace Snake
 {
-    class VerticalLine
+    class VerticalLine : Figure
     {
-        List<Point> pList;
-
         public VerticalLine(int yTop, int yBottom, int x, char sym)
         {
             pList = new List<Point>();
@@ -17,14 +15,6 @@ namespace Snake
                 pList.Add(p);
             }
 
-        }
-
-        public void Drow()
-        {
-            foreach (Point p in pList)
-            {
-                p.Draw();
-            }
         }
     }
 }
